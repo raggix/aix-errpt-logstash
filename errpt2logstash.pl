9a1e0c8d54ffedeb1cf4c8aa1826482f04754b50
raggix <rwellnitz@gmx.de>
2015-01-28 23:02:08 +0100

    Bugfixing, improved argument parsing

--- errpt2logstash.pl
+++ errpt2logstash.pl
@@ -5,7 +5,7 @@
 #
 # Description   : Forward (and transform) ERRPT events to a logstash server.
 #
-# Version       : 0.1
+# Version       : 0.2
 #-------------------------------------------------------------------------------
 #
 # History:
@@ -13,6 +13,7 @@
 # <DATE>        <AUTHOR>        <REASON>
 # ----------    -------------- ------------------------------------------------
 # 2015-01-26    Ron Wellnitz    initial creation
+# 2015-01-28    Ron Wellnitz    Bugfixing, improved argument parsing
 #
 #
 # ToDo:
@@ -64,17 +65,13 @@
 #-------------------------------------------------------------------------------
 # [Example] /etc/errpt2logstash.conf
 #-------------------------------------------------------------------------------
-# ># remote logstash server name
 # >logstash_server_name=localhost
-# >#
-# ># remote logstash server port
 # >logstash_server_port=5555
-# >#
+# >ignore_syslog_messages=0
 # ># ignore messages from AIX Syslog, e.g. they already/directly forwarded
 # ># from Syslog-Daemon to the logstash server -> avoid duplicate log entrys
 # ># 0 = false
 # ># 1 = true
-# >ignore_syslog_messages=0
 #
 #-------------------------------------------------------------------------------
 # [Example] logstash-input.conf
@@ -122,15 +119,13 @@ my $CONFIGFILE = "/etc/errpt2logstash.conf";
 # errnotify arguments
 my $ELEMENT;
 my %ELEMENTS;
-$ELEMENTS{sequence_number} = shift;
-$ELEMENTS{error_id}        = shift;
-$ELEMENTS{error_class}     = shift;
-$ELEMENTS{error_type}      = shift;
-$ELEMENTS{alert_flags}     = shift;
-$ELEMENTS{resource_name}   = shift;
-$ELEMENTS{resource_type}   = shift;
-$ELEMENTS{resource_class}  = shift;
-$ELEMENTS{error_label}     = shift;
+foreach ("errpt_sequence_number", "errpt_error_id", "errpt_class",
+         "errpt_type",            "errpt_alert_flags", "errpt_resource_name",
+         "errpt_resource_type",   "errpt_resource_class", "errpt_error_label") {
+
+  $ELEMENTS{$_} = ($#ARGV + 1) ? shift : 'unset';
+}
+
 
 # errpt output
 $ELEMENTS{message}         = "";
@@ -152,26 +147,19 @@ $ERROR_CLASS{O}           = "Operator Notice";
 $ERROR_CLASS{U}           = "Undetermined";
 
 # initial user configuration variables
-my $SERVER_ADDR            = "";
-my $SERVER_PORT            = "";
-my $IGNORE_SYSLOG          = 0;
+my $SERVER_ADDR           = "";
+my $SERVER_PORT           = "";
+my $IGNORE_SYSLOG         = 0;
 
 #===============================================================================
 # main program
 #===============================================================================
 
 #
-# - if set, ignore syslog messages
-#
-if ($IGNORE_SYSLOG && $ELEMENTS{error_label} == "SYSLOG") {
-  exit(0);
-}
-
-#
 # - parse configuration file
 #
 open(FILE, $CONFIGFILE)
-  or die "\nERROR: Unable to open config file: $CONFIGFILE!\n";
+  or die "\nERROR: Unable to open configuration file: $CONFIGFILE!\n";
 while(<FILE>) {
    chomp($_);
    if ($_ =~ /logstash_server_name/) {
@@ -184,6 +172,14 @@ while(<FILE>) {
       (my $dummy,$IGNORE_SYSLOG) = split(/=/,$_);
    }
 }
+
+#
+# - if set, ignore syslog messages
+#
+if ($IGNORE_SYSLOG && $ELEMENTS{errpt_error_label} eq "SYSLOG") {
+  exit(0);
+}
+
 if(!$SERVER_ADDR || !$SERVER_PORT) {
   print "\nERROR: Invalid server: $SERVER_ADDR or port: $SERVER_PORT!\n";
   exit(1);
@@ -195,29 +191,34 @@ if(!$SERVER_ADDR || !$SERVER_PORT) {
 # - remove/escape special chars
 #
 chomp($ELEMENTS{logsource} = `/usr/bin/hostname -s 2>&1`);
-my @ERRPT_OUT = `/usr/bin/errpt -l $ELEMENTS{sequence_number} -a 2>&1`;
-foreach(@ERRPT_OUT) {
- chomp($_);
- $_ =~ s/[\t]/\\t/g;
- $_ =~ s/(['"])/\\\$1/g;
- #$_ =~ s/[^a-zA-z0-9:\/+-=?!.,;_#()<> ]/ /g;
- $ELEMENTS{message} .= "$_\\r\\n";
+
+if ($ELEMENTS{errpt_sequence_number} =~ /^\d+$/) {
+  my @ERRPT_OUT = `/usr/bin/errpt -l $ELEMENTS{errpt_sequence_number} -a 2>&1`;
+  foreach(@ERRPT_OUT) {
+    chomp($_);
+    $_ =~ s/[\t]/\\t/g;
+    $_ =~ s/(['"])/\\\$1/g;
+    #$_ =~ s/[^a-zA-z0-9:\/+-=?!.,;_#()<> ]/ /g;
+    $ELEMENTS{message} .= "$_\\r\\n";
+  }
+} else {
+  $ELEMENTS{message} .= "invalid sequence number\\r\\n";
 }
 
 #
 # - do some mapping for a better standard syslog matching
 #
-if (exists $ERROR_CLASS{$ELEMENTS{error_class}}) {
-  $ELEMENTS{facility_label} = $ERROR_CLASS{$ELEMENTS{error_class}};
+if (exists $ERROR_CLASS{$ELEMENTS{errpt_class}}) {
+  $ELEMENTS{facility_label} = $ERROR_CLASS{$ELEMENTS{errpt_class}};
 } else {
-  $ELEMENTS{facility_label} = $ELEMENTS{error_class};
+  $ELEMENTS{facility_label} = $ELEMENTS{errpt_class};
 }
-if (exists $ERROR_TYPE{$ELEMENTS{error_type}}) {
-  $ELEMENTS{severity_label} = $ERROR_TYPE{$ELEMENTS{error_type}};
+if (exists $ERROR_TYPE{$ELEMENTS{errpt_type}}) {
+  $ELEMENTS{severity_label} = $ERROR_TYPE{$ELEMENTS{errpt_type}};
 } else {
-  $ELEMENTS{severity_label} = $ELEMENTS{error_type};
+  $ELEMENTS{severity_label} = $ELEMENTS{errpt_type};
 }
-$ELEMENTS{program} = $ELEMENTS{error_label};
+$ELEMENTS{program} = $ELEMENTS{errpt_error_label};
 
 #
 # - open tcp connection to logstash server
